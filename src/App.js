05805071e57c3377569853e8870e8c532ed438c8
Timetime0 <75958612+Timetime0@users.noreply.github.com>
2021-08-09 10:20:09 +0700

    09/08 lazyloading

--- src/App.js
+++ src/App.js
@@ -1,75 +1,82 @@
-import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Header from './Components/Header/Header';
-import Footer from './Components/Footer/Footer';
-import { ThemeProvider } from 'styled-components'
-import {darkTheme} from './StyledComponent/theme/darkTheme'
-import Waiting from './Pages/Waiting/Waiting';
-import {clientRouter} from './config/router.config'
-import { connect } from 'react-redux';
-import { Component } from 'react';
-import { LOGIN_ADMIN, LOGIN_USER } from './Redux/Types/auth-type';
-import BackToTop from './Components/BackToTop/BackToTop';
-import ScrollToTop from './Components/BackToTop/ScrollToTop';
-
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import { ThemeProvider } from "styled-components";
+import { darkTheme } from "./StyledComponent/theme/darkTheme";
+import Waiting from "./Pages/Waiting/Waiting";
+import { clientRouter } from "./config/router.config";
+import { connect } from "react-redux";
+import { Component } from "react";
+import { LOGIN_ADMIN, LOGIN_USER } from "./Redux/Types/auth-type";
+import BackToTop from "./Components/BackToTop/BackToTop";
+import ScrollToTop from "./Components/BackToTop/ScrollToTop";
+import { Suspense } from "react";
 
 class App extends Component {
-  
-  
   RenderRouterList = () => {
-    return clientRouter.map((item,index)=>{
-      return  <Route key={index} exact={item.exact} path={item.path} component={item.component}/>
-    })    
-  }
-  
-  render(){
+    return clientRouter.map((item, index) => {
+      return (
+        <Route
+          key={index}
+          exact={item.exact}
+          path={item.path}
+          component={item.component}
+        />
+      );
+    });
+  };
+
+  render() {
     return (
       <ThemeProvider theme={darkTheme}>
         <BrowserRouter>
-          <ScrollToTop/>
+          <ScrollToTop />
           <Header />
-          <Waiting/>
-          <Switch>
-            {this.RenderRouterList()}
-          </Switch>
-          <BackToTop/>
+          <Waiting />
+          <Suspense
+            fallback={
+              <div>
+                {" "}
+                <Waiting />
+              </div>
+            }
+          >
+            <Switch>{this.RenderRouterList()}</Switch>
+          </Suspense>
+          <BackToTop />
           <Footer />
         </BrowserRouter>
       </ThemeProvider>
-  
     );
   }
-  
+
   getUserFromLocal = () => {
-    const userAdmin = localStorage.getItem('admin')
-    const userClient = localStorage.getItem('client')
-    if(userAdmin){
+    const userAdmin = localStorage.getItem("admin");
+    const userClient = localStorage.getItem("client");
+    if (userAdmin) {
       this.props.dispatch({
-        type:LOGIN_ADMIN,
-        data:JSON.parse(userAdmin)
-      })
-    } 
-    if(userClient){
+        type: LOGIN_ADMIN,
+        data: JSON.parse(userAdmin),
+      });
+    }
+    if (userClient) {
       this.props.dispatch({
         type: LOGIN_USER,
-        data: JSON.parse(userClient)
-      })
+        data: JSON.parse(userClient),
+      });
     }
+  };
 
-  } 
-
-  componentDidMount(){
-    this.getUserFromLocal()
+  componentDidMount() {
+    this.getUserFromLocal();
   }
 }
 
-const mapStateToProps = (state)=>{
-  return{
-    admin: state.userReducer.user.admin
-  }
-}
+const mapStateToProps = (state) => {
+  return {
+    admin: state.userReducer.user.admin,
+  };
+};
 
-
-export default connect(mapStateToProps) (App);
-
-
+export default connect(mapStateToProps)(App);
--- src/config/router.config.js
+++ src/config/router.config.js
@@ -1,94 +1,97 @@
-import Carousel from "../Components/Carousel/Carousel";
-import RapChieuPhim from "../Components/RapChieuPhim/RapChieuPhim";
-import Admin from "../Pages/Admin/Admin";
-import AdminLogin from "../Pages/Admin/AdminLogin";
-import Booking from "../Pages/ChiTietPhim/Booking";
-import ChiTietPhim from "../Pages/ChiTietPhim/ChiTietPhim";
-import Brand from "../Pages/Footer/Brand";
-import ChinhSachBaoMat from "../Pages/Footer/ChinhSachBaoMat";
-import FAQ from "../Pages/Footer/FAQ";
-import ThaoThuanSuDung from "../Pages/Footer/ThaoThuanSuDung";
-import Home from "../Pages/Home/Home";
-import Login from "../Pages/Login/Login";
-import PageNotFound from "../Pages/PageNotFound/PageNotFound";
-import UserProfile from '../Pages/User/UserProfile'
+import { lazy } from "react";
+const RapChieuPhim = lazy(() =>
+  import("../Components/RapChieuPhim/RapChieuPhim")
+);
+const Admin = lazy(() => import("../Pages/Admin/Admin"));
+const AdminLogin = lazy(() => import("../Pages/Admin/AdminLogin"));
+const Booking = lazy(() => import("../Pages/ChiTietPhim/Booking"));
+const ChiTietPhim = lazy(() => import("../Pages/ChiTietPhim/ChiTietPhim"));
+const Brand = lazy(() => import("../Pages/Footer/Brand"));
+const ChinhSachBaoMat = lazy(() => import("../Pages/Footer/ChinhSachBaoMat"));
+const FAQ = lazy(() => import("../Pages/Footer/FAQ"));
+const ThaoThuanSuDung = lazy(() => import("../Pages/Footer/ThaoThuanSuDung"));
+const Home = lazy(() => import("../Pages/Home/Home"));
+const Login = lazy(() => import("../Pages/Login/Login"));
+const PageNotFound = lazy(() => import("../Pages/PageNotFound/PageNotFound"));
+const UserProfile = lazy(() => import("../Pages/User/UserProfile"));
+const Carousel = lazy(() => import("../Components/Carousel/Carousel"));
+
+
 export const clientRouter = [
-    {
-        path: "/admin",
-        exact: true,
-        component: Admin
-    },
+  {
+    path: "/admin",
+    exact: true,
+    component: Admin,
+  },
 
-    {
-        path: "/admin/login",
-        exact: true,
-        component: AdminLogin
-    },
-    {
-        path: "/",
-        exact: true,
-        component: Home
-    },
-    {
-        path: "/home",
-        exact: true,
-        component: Home
-    },
-    {
-        path: "/carousel",
-        exact: true,
-        component: Carousel
-    },
-    {
-        path: "/login",
-        exact: true,
-        component: Login
-    },
-    {
-        path: "/profile",
-        exact: true,
-        component: UserProfile
-    },
-    {
-        path: "/rapchieuphim",
-        exact: true,
-        component: RapChieuPhim
-    },
-    {
-        path: "/chitietphim/:maPhim",
-        exact: true,
-        component: ChiTietPhim
-    },
-    {
-        path: "/booking/:maLichChieu",
-        exact: true,
-        component: Booking
-    },
-    {
-        path: "/brand",
-        exact: true,
-        component: Brand
-    },
-    {
-        path: "/noidung/thoathuan",
-        exact: true,
-        component: ThaoThuanSuDung
-    },
-    {
-        path: "/noidung/baomat",
-        exact: true,
-        component: ChinhSachBaoMat
-    },
-    {
-        path: "/noidung/faq",
-        exact: true,
-        component: FAQ
-    },
-    {
-        path: "*",
-        exact: true,
-        component: PageNotFound
-    },
-
-
-]
+  {
+    path: "/admin/login",
+    exact: true,
+    component: AdminLogin,
+  },
+  {
+    path: "/",
+    exact: true,
+    component: Home,
+  },
+  {
+    path: "/home",
+    exact: true,
+    component: Home,
+  },
+  {
+    path: "/carousel",
+    exact: true,
+    component: Carousel,
+  },
+  {
+    path: "/login",
+    exact: true,
+    component: Login,
+  },
+  {
+    path: "/profile",
+    exact: true,
+    component: UserProfile,
+  },
+  {
+    path: "/rapchieuphim",
+    exact: true,
+    component: RapChieuPhim,
+  },
+  {
+    path: "/chitietphim/:maPhim",
+    exact: true,
+    component: ChiTietPhim,
+  },
+  {
+    path: "/booking/:maLichChieu",
+    exact: true,
+    component: Booking,
+  },
+  {
+    path: "/brand",
+    exact: true,
+    component: Brand,
+  },
+  {
+    path: "/noidung/thoathuan",
+    exact: true,
+    component: ThaoThuanSuDung,
+  },
+  {
+    path: "/noidung/baomat",
+    exact: true,
+    component: ChinhSachBaoMat,
+  },
+  {
+    path: "/noidung/faq",
+    exact: true,
+    component: FAQ,
+  },
+  {
+    path: "*",
+    exact: true,
+    component: PageNotFound,
+  },
+];
